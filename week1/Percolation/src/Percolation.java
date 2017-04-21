715cf30015fcb2b56e15be049de3bd894657d08c
Haibo Yu <sealyu@hotmail.com>
2017-04-21 08:59:34 -0400

    Programming

---
+++ week1/Percolation/src/Percolation.java
@@ -0,0 +1,26 @@
+/**
+ * @author Haibo Yu on 02/24/2017.
+ */
+public class Percolation {
+    public Percolation(int n) {
+
+    }
+    public void open(int row, int col) {
+
+    }
+    public boolean isOpen(int row, int col) {
+
+    }
+    public boolean isFull(int row, int col){
+
+    }
+    public int numberOfOpenSites(){
+
+    }
+    public boolean percolates() {
+
+    }
+    public static void main(String[] args) {
+
+    }
+}
---
+++ week1/Percolation/src/PercolationStats.java
@@ -0,0 +1,23 @@
+/**
+ * @author Haibo Yu on 02/24/2017.
+ */
+public class PercolationStats {
+    public PercolationStats(int n, int trials) {
+
+    }
+    public double mean() {
+
+    }
+    public double stddev() {
+
+    }
+    public double confidenceLo() {
+
+    }
+    public double confidenceHi() {
+
+    }
+    public static void main(String[] args){
+
+    }
+}
