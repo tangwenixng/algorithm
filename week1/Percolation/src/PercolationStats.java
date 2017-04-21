6a2c132303c9a27844b539c0a3fb2ed5b0ec5054
Haibo Yu <sealyu@hotmail.com>
2017-04-21 16:13:41 -0400

    Add jar

--- week1/Percolation/src/PercolationStats.java
+++ week1/Percolation/src/PercolationStats.java
@@ -6,16 +6,16 @@ public class PercolationStats {
 
     }
     public double mean() {
-
+        return 0.0;
     }
     public double stddev() {
-
+        return 0.0;
     }
     public double confidenceLo() {
-
+        return 0.0;
     }
     public double confidenceHi() {
-
+        return 0.0;
     }
     public static void main(String[] args){
 
