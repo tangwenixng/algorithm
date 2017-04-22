41113e222c1abf81b483d25989a0eae34f6ce921
Haibo Yu <sealyu@hotmail.com>
2017-04-22 00:26:45 -0400

    Finish week1

--- week1/Percolation/src/Percolation.java
+++ week1/Percolation/src/Percolation.java
@@ -1,3 +1,5 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 /**
@@ -5,16 +7,13 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
  */
 public class Percolation {
 
-    //The number of the grid
+    // The number of the grid
     private int siteSize;
 
-    //The 2-dim array to store the open status for every site
+    // The 2-dim array to store the open status for every site
     private boolean[][] openStatus;
 
-    //The 2-dim array to store the full status for every site
-    private boolean[][] fullStatus;
-
-    //The number of open sites
+    // The number of open sites
     private int numOfOpenSites;
 
     private WeightedQuickUnionUF siteOperator;
@@ -23,14 +22,57 @@ public class Percolation {
     public Percolation(int n) {
         if (n <= 0) throw new IllegalArgumentException("Error: The number of site must big than 0!");
         siteSize = n;
+        numOfOpenSites = 0;
 
-        siteOperator = new WeightedQuickUnionUF(siteSize);
-        openStatus = new boolean[siteSize][siteSize];
-        fullStatus = new boolean[siteSize][siteSize];
+        siteOperator = new WeightedQuickUnionUF(siteSize*siteSize+2);
+
+        openStatus = new boolean[siteSize+1][siteSize+1];
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < n + 1; j++) {
+                openStatus[i][j] = false;
+            }
+        }
     }
-    public void open(int row, int col) {
-        validateSitePosition(row,col);
 
+    /**
+     * open the site if it's still not open
+     * @param row
+     * @param col
+     */
+    public void open(int row, int col) {
+        validateSitePosition(row, col);
+        if (openStatus[row][col]) {
+            return;
+        }
+
+        numOfOpenSites++;
+
+        openStatus[row][col] = true;
+
+        // Connect to top if row>1
+        if (row > 1 && openStatus[row - 1][col]) {
+            siteOperator.union((row - 1) * siteSize + col, (row - 2) * siteSize + col);
+        }
+        // Connect to bottom if row<siteSize
+        if (row < siteSize && openStatus[row + 1][col]) {
+            siteOperator.union((row - 1) * siteSize + col, (row) * siteSize + col);
+        }
+        // Connect to left if col>1
+        if (col > 1 && openStatus[row][col - 1]) {
+            siteOperator.union((row - 1) * siteSize + col, (row - 1) * siteSize + col - 1);
+        }
+        // Connect to right if col<siteSize
+        if (col < siteSize && openStatus[row][col + 1]) {
+            siteOperator.union((row - 1) * siteSize + col, (row - 1) * siteSize + col + 1);
+        }
+        // Connect to top virtual site if row=1
+        if (row == 1) {
+            siteOperator.union((row - 1) * siteSize + col, 0);
+        }
+        // Connect to bottom virtual site if row=siteSize
+        if (row == siteSize) {
+            siteOperator.union((row - 1) * siteSize + col, siteSize * siteSize + 1);
+        }
     }
 
     /**
@@ -40,8 +82,8 @@ public class Percolation {
      * @return The open status of the site
      */
     public boolean isOpen(int row, int col) {
-        validateSitePosition(row,col);
-        return openStatus[row - 1][col - 1];
+        validateSitePosition(row, col);
+        return openStatus[row][col];
     }
 
     /**
@@ -50,9 +92,9 @@ public class Percolation {
      * @param col The col
      * @return The full status of the site
      */
-    public boolean isFull(int row, int col){
-        validateSitePosition(row,col);
-        return fullStatus[row - 1][col - 1];
+    public boolean isFull(int row, int col) {
+        validateSitePosition(row, col);
+        return siteOperator.connected((row - 1) * siteSize + col, 0);
     }
 
     /**
@@ -60,8 +102,8 @@ public class Percolation {
      * @param row The row
      * @param col The column
      */
-    private void validateSitePosition(int row, int col){
-        if(row <0 || row >siteSize || col <0 || col > siteSize){
+    private void validateSitePosition(int row, int col) {
+        if (row <= 0 || row > siteSize || col <= 0 || col > siteSize) {
             throw new IndexOutOfBoundsException("The row number and column number must be less than total number!");
         }
     }
@@ -71,19 +113,29 @@ public class Percolation {
      * The number of open sites
      * @return The number of open sties
      */
-    public int numberOfOpenSites(){
+    public int numberOfOpenSites() {
         return numOfOpenSites;
     }
 
     /**
-     * Check if the sites percolates
+     * Check if the sites percolates, when the top virtual node connect
+     * to the bottom birtual node means it percolates
      * @return The status of if it percolate
      */
     public boolean percolates() {
-        return false;
+        return siteOperator.connected(siteSize * siteSize + 1, 0);
     }
 
     public static void main(String[] args) {
-
+        int size = StdIn.readInt();
+        Percolation percolation = new Percolation(size);
+        while (!StdIn.isEmpty()) {
+            int row = StdIn.readInt();
+            int col = StdIn.readInt();
+            if (!percolation.isOpen(row, col)) {
+                percolation.open(row, col);
+                StdOut.println(row + " " + col);
+            }
+        }
     }
 }
--- week1/Percolation/src/PercolationStats.java
+++ week1/Percolation/src/PercolationStats.java
@@ -1,26 +1,63 @@
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
 /**
  * @author Haibo Yu on 02/24/2017.
  */
 public class PercolationStats {
+    // Array to store the threhold values for every experiment
+    private double[] thresholds;
+
     public PercolationStats(int n, int trials) {
-        if(trials <=0 || n<=0){
+        if (trials <= 0 || n <= 0) {
             throw new java.lang.IllegalArgumentException("Error: n and trials must big than 0!");
+        }
+        thresholds = new double[trials];
+        for (int i = 0; i < trials; i++) {
+            thresholds[i] = 0;
+        }
+        for (int i = 0; i < trials; i++) {
+            Percolation perc = new Percolation(n);
+            while (!perc.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                if (!perc.isOpen(row, col)) {
+                    thresholds[i]++;
+                    perc.open(row, col);
+                }
+            }
+            thresholds[i] = thresholds[i] / (n * n);
         }
 
     }
     public double mean() {
-        return 0.0;
+        return StdStats.mean(thresholds);
     }
     public double stddev() {
-        return 0.0;
+        return StdStats.stddev(thresholds);
     }
     public double confidenceLo() {
-        return 0.0;
+        double mean = mean();
+        double stddev = stddev();
+        double temp = 1.96 * stddev
+                / java.lang.StrictMath.pow(thresholds.length, 1.0 / 2);
+        return mean - temp;
     }
     public double confidenceHi() {
-        return 0.0;
+        double mean = mean();
+        double stddev = stddev();
+        double temp = 1.96 * stddev
+                / java.lang.StrictMath.pow(thresholds.length, 1.0 / 2);
+        return mean + temp;
     }
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        int n = Integer.parseInt(args[0]);
+        int trials = Integer.parseInt(args[1]);
+        PercolationStats percStates = new PercolationStats(n, trials);
+        StdOut.println("mean = " + percStates.mean());
+        StdOut.println("stddev = " + percStates.stddev());
+        StdOut.println("95% confidence interval = " + percStates.confidenceLo()
+                + ", " + percStates.confidenceHi());
     }
 }
