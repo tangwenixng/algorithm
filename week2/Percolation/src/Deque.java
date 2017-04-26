9e47eb5f2ba56fe04d680beb497323c802927aee
Haibo Yu <sealyu@hotmail.com>
2017-04-25 21:56:39 -0400

    Initialize week 2

---
+++ week2/Percolation/src/Deque.java
@@ -0,0 +1,34 @@
+import java.util.Iterator;
+
+/**
+ * @author sealyu on 25/04/2017.
+ */
+public class Deque<Item> implements Iterable<Item> {
+    public Deque(){
+
+    }
+    public boolean isEmpty(){
+
+    }
+    public int size(){
+
+    }
+    public void addFirst(Item item){
+
+    }
+    public void addLast(Item item){
+
+    }
+    public Item removeFirst(){
+
+    }
+    public Item removeLast(){
+
+    }
+    public Iterator<Item> iterator(){
+
+    }
+    public static void main(String[] args){
+
+    }
+}
---
+++ week2/Percolation/src/Permutation.java
@@ -0,0 +1,10 @@
+import edu.princeton.cs.algs4.StdIn;
+
+/**
+ * @author sealyu on 25/04/2017.
+ */
+public class Permutation {
+    public static void main(String[] args) {
+        int size = StdIn.readInt();
+    }
+}
